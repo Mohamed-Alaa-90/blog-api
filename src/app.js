dc71810f327f67b965b2ac06bfe7b243100ba9aa
Mohamed_Alaa <m7md3laa500@gmail.com>
2026-09-18 05:26:57 +0300

    feat: connect to database

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
-import dotenv from "dotenv";
-dotenv.config();
-
+import "./config/env.js";
 import express from "express";
+import pool from "./config/db.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 //dbConnection
-
+try {
+  const connection = await pool.getConnection();
+  connection.release();
+  console.log("db connected");
+} catch (error) {
+  console.error("Error connecting to the database:", error);
+}
 app.use(express.json());
 
 app.get("/", (req, res) => {
