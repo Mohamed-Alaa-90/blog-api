50d800ebdaa988ecd000402926c27d1f5a0c6c6a
Mohamed_Alaa <74032994+Mohamed-Alaa-90@users.noreply.github.com>
2026-09-21 21:47:07 +0200

    feat: add login route with validation

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,13 +1,14 @@
 import express from "express";
 import {
   registerUserSchema,
-  
+  loginUserSchema,
 } from "../validators/users.validator.js";
 import { validate } from "../middlewares/validate.middleware.js";
-import {  register } from "../controllers/auth.controller.js";
+import { login, register } from "../controllers/auth.controller.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/register", validate(registerUserSchema), register);
 
+authRouter.post("/login", validate(loginUserSchema), login);
 export default authRouter;
